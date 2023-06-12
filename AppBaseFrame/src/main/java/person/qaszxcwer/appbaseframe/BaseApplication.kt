b57da3f036fcf43872b747148d8ec2d7fdd306c0
qaszxcwer <48939745+qaszxcwer@users.noreply.github.com>
2023-06-12 23:52:59 +0800

    统一各种sdk版本

--- AppBaseFrame/src/main/java/person/qaszxcwer/appbaseframe/BaseApplication.kt
+++ AppBaseFrame/src/main/java/person/qaszxcwer/appbaseframe/BaseApplication.kt
@@ -8,7 +8,7 @@ import com.appjoint2.annotations.ModuleSpec
  * date: 2023/5/6
  * 
  */
-@ModuleSpec(priority = 1)
+@ModuleSpec(priority = 0)
 // todo 为什么作为父类也要加注解？否则生命周期不执行？等待AppJoint2作者解答
 open class BaseApplication: Application() {
     companion object {
